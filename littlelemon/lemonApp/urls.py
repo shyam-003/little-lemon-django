ed11c428ab47ab208c05144ad983688f02df7dbc
Shyam <shyam.03@icloud.com>
2025-01-23 01:33:18 +0530

    added api endpoints for menu and booking

--- littlelemon/lemonApp/urls.py
+++ littlelemon/lemonApp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('menu/<int:pk>', views.MenuItemsDetailView.as_view(), name='menu_detail'),
 ]
--- littlelemon/lemonApp/views.py
+++ littlelemon/lemonApp/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html',{})
+
+
+from .models import *
+from .serializers import *
+from rest_framework import generics, viewsets
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class MenuItemsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
