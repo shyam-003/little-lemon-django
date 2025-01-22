a0faca7cb9f92ec6296791fc9eba6eb4a0c349f6
Shyam <shyam.03@icloud.com>
2025-01-23 00:56:40 +0530

    created booking and menu models

--- littlelemon/lemonApp/models.py
+++ littlelemon/lemonApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    guests = models.IntegerField()
+    booking_data = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} - {self.no_of_guests} guests on {self.booking_date}"
+
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} - ${self.price} ({self.inventory} in stock)"
